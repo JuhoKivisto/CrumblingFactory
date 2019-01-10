9320d94ef5714b6989de074f9f3b9991967e155e
chungphamhn <chungphamhn@gmail.com>
2019-01-10 21:46:47 +0200

    pick up object

---
+++ Assets/HandController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
+    private SteamVR_TrackedObject trackedObject;
+
+    private GameObject pickUp;
+
+
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(controller == null) {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+
+        if (controller.GetPressDown(gripButton) && pickUp != null) {
+            pickUp.transform.parent = this.transform;
+            pickUp.GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        if (controller.GetPressUp(gripButton) && pickUp != null) {
+            pickUp.transform.parent = null;
+            pickUp.GetComponent<Rigidbody>().useGravity = true;
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        pickUp = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        pickUp = null;
+    }
+}
---
+++ Assets/InteractableItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+
+    private bool currentlyInteracting;
+
+    private HandController attachedHand;
+
+    private Transform interactingPoint;
+	// Use this for initialization
+	void Start () {
+
+        rigidbody = GetComponent<Rigidbody>();
+        interactingPoint = new GameObject().transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BeginInteraction(HandController hand) {
+        attachedHand = hand;
+        interactingPoint.position = hand.transform.position;
+        interactingPoint.rotation = hand.transform.rotation;
+        interactingPoint.SetParent(transform, true);
+
+        currentlyInteracting = true;
+    }
+
+    public void EndInteraction(HandController hand) {
+
+    }
+}
