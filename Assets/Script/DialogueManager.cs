f4c774925b26c4b648d3346e15d28618b8ac1c90
Bbikrampangeni <33446541+Bbikrampangeni@users.noreply.github.com>
2019-01-12 18:01:51 +0200

    Dialogue system
    A simple dialogue system / info about the game panel with the typing effect text

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    /* keeps track off all the sentence used in the DIalogue*/
+    private Queue<string> sentences;  /* Queue uses a FIFO system */
+
+
+    public Text nametext;
+    public Text dialogueText;
+
+	// Use this for initialization
+	void Start () {
+
+        sentences = new Queue<string>(); /* for initialization  */
+		
+	}
+
+
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log ("starting:" + dialogue.name);
+        nametext.text = dialogue.name;
+
+
+        sentences.Clear();
+
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplaNextSentence();
+
+    }
+
+    public void DisplaNextSentence()
+    {
+        if (sentences.Count == 0) /* if there are no dilaogues left then end the function */
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();  /* if the user presses continue before all the letter in the last dialogue is completely animated , it stops the function and starts the new dialgue*/
+        StartCoroutine(TypeSentence(sentence)); /* animates the next dialoge */
+    }
+    IEnumerator TypeSentence(String sentence) /* helps to print the text letters one by onne */
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null; /* the animation takes the single frame to load */
+        }
+
+
+    }
+    public  void EndDialogue()
+    {
+        Debug.Log("End of the dialogue"); 
+    }
+}
