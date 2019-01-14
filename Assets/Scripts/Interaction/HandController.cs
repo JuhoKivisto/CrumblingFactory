00ba3376deb42ef063aa5c58b894f0284e6a0e8e
chungphamhn <chungphamhn@gmail.com>
2019-01-14 15:01:36 +0200

    test

--- Assets/Scripts/Interaction/HandController.cs
+++ Assets/Scripts/Interaction/HandController.cs
@@ -4,77 +4,77 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour {
 
-    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    //private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    //private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 
-    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }        //get hand sets
-    private SteamVR_TrackedObject trackedObj;
+    //private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }        //get hand sets
+    //private SteamVR_TrackedObject trackedObj;
 
-    HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
+    //HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
 
-    private InteractableItem closestItem;
-    private InteractableItem interactingItem;
+    //private InteractableItem closestItem;
+    //private InteractableItem interactingItem;
 
-    // Use this for initialization
-    void Start() {
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
-        Debug.Log(objectsHoveringOver);
-    }
+    //// Use this for initialization
+    //void Start() {
+    //    trackedObj = GetComponent<SteamVR_TrackedObject>();
+    //    Debug.Log(objectsHoveringOver);
+    //}
 
-    // Update is called once per frame
-    void Update() {
-        if (controller == null) {
-            Debug.Log("Controller not initialized");
-            return;
-        }
+    //// Update is called once per frame
+    //void Update() {
+    //    if (controller == null) {
+    //        Debug.Log("Controller not initialized");
+    //        return;
+    //    }
 
-        if (controller.GetPressDown(gripButton)) {
-            float minDistance = float.MaxValue;
+    //    if (controller.GetPressDown(gripButton)) {
+    //        float minDistance = float.MaxValue;
 
-            Debug.Log("min distance " + minDistance);
+    //        Debug.Log("min distance " + minDistance);
 
-            float distance;
-            foreach (InteractableItem item in objectsHoveringOver) {                        //get all interactable items
-                distance = (item.transform.position - transform.position).sqrMagnitude;     //get distance between controller and object
+    //        float distance;
+    //        foreach (InteractableItem item in objectsHoveringOver) {                        //get all interactable items
+    //            distance = (item.transform.position - transform.position).sqrMagnitude;     //get distance between controller and object
 
-                Debug.Log("distance " + distance);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    closestItem = item;
-                }
-            }
+    //            Debug.Log("distance " + distance);
+    //            if (distance < minDistance) {
+    //                minDistance = distance;
+    //                closestItem = item;
+    //            }
+    //        }
 
-            interactingItem = closestItem;
+    //        interactingItem = closestItem;
 
-            if (interactingItem) {
-                if (interactingItem.IsInteracting()) {
-                    interactingItem.EndInteraction(this);
-                }
+    //        if (interactingItem) {
+    //            if (interactingItem.IsInteracting()) {
+    //                interactingItem.EndInteraction(this);
+    //            }
 
-                interactingItem.BeginInteraction(this);
-            }
-        }
+    //            interactingItem.BeginInteraction(this);
+    //        }
+    //    }
 
-        if (controller.GetPressUp(gripButton) && interactingItem != null) {
-            interactingItem.EndInteraction(this);
-        }
-    }
+    //    if (controller.GetPressUp(gripButton) && interactingItem != null) {
+    //        interactingItem.EndInteraction(this);
+    //    }
+    //}
 
-    private void OnTriggerEnter(Collider collider) {                                            //when controller collides with object
-        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
-        if (collidedItem) {
-            objectsHoveringOver.Add(collidedItem);
-        }
-    }
+    //private void OnTriggerEnter(Collider collider) {                                            //when controller collides with object
+    //    InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+    //    if (collidedItem) {
+    //        objectsHoveringOver.Add(collidedItem);
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider collider) {                                             //when controller exits object                    
-        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
-        if (collidedItem) {
-            objectsHoveringOver.Remove(collidedItem);
-        }
-    }
+    //private void OnTriggerExit(Collider collider) {                                             //when controller exits object                    
+    //    InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+    //    if (collidedItem) {
+    //        objectsHoveringOver.Remove(collidedItem);
+    //    }
+    //}
 
-    /*
+ 
 
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
@@ -90,7 +90,7 @@ public class HandController : MonoBehaviour {
         trackedObject = GetComponent<SteamVR_TrackedObject>();
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		if(controller == null) {
             Debug.Log("Controller not initialized");
@@ -100,12 +100,12 @@ public class HandController : MonoBehaviour {
 
         if (controller.GetPressDown(gripButton) && pickUp != null) {
             pickUp.transform.parent = this.transform;
-            pickUp.GetComponent<Rigidbody>().useGravity = false;
+            pickUp.GetComponent<Rigidbody>().isKinematic = true;
         }
 
         if (controller.GetPressUp(gripButton) && pickUp != null) {
             pickUp.transform.parent = null;
-            pickUp.GetComponent<Rigidbody>().useGravity = true;
+            pickUp.GetComponent<Rigidbody>().isKinematic = false;
         }
 
 
@@ -117,5 +117,5 @@ public class HandController : MonoBehaviour {
 
     private void OnTriggerExit(Collider other) {
         pickUp = null;
-    }*/
+    }
 }
