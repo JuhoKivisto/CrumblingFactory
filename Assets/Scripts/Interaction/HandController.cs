e79602c1f2c903344bb96743c4425865fe1593b9
chungphamhn <chungphamhn@gmail.com>
2019-01-14 14:41:39 +0200

    test

--- Assets/Scripts/Interaction/HandController.cs
+++ Assets/Scripts/Interaction/HandController.cs
@@ -6,6 +6,78 @@ public class HandController : MonoBehaviour {
 
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }        //get hand sets
+    private SteamVR_TrackedObject trackedObj;
+
+    HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
+
+    private InteractableItem closestItem;
+    private InteractableItem interactingItem;
+
+    // Use this for initialization
+    void Start() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        Debug.Log(objectsHoveringOver);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (controller == null) {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        if (controller.GetPressDown(gripButton)) {
+            float minDistance = float.MaxValue;
+
+            Debug.Log("min distance " + minDistance);
+
+            float distance;
+            foreach (InteractableItem item in objectsHoveringOver) {                        //get all interactable items
+                distance = (item.transform.position - transform.position).sqrMagnitude;     //get distance between controller and object
+
+                Debug.Log("distance " + distance);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestItem = item;
+                }
+            }
+
+            interactingItem = closestItem;
+
+            if (interactingItem) {
+                if (interactingItem.IsInteracting()) {
+                    interactingItem.EndInteraction(this);
+                }
+
+                interactingItem.BeginInteraction(this);
+            }
+        }
+
+        if (controller.GetPressUp(gripButton) && interactingItem != null) {
+            interactingItem.EndInteraction(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider) {                                            //when controller collides with object
+        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+        if (collidedItem) {
+            objectsHoveringOver.Add(collidedItem);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider) {                                             //when controller exits object                    
+        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+        if (collidedItem) {
+            objectsHoveringOver.Remove(collidedItem);
+        }
+    }
+
+    /*
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
     
 
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
@@ -45,5 +117,5 @@ public class HandController : MonoBehaviour {
 
     private void OnTriggerExit(Collider other) {
         pickUp = null;
-    }
+    }*/
 }
--- Assets/Scripts/Interaction/InteractableItem.cs
+++ Assets/Scripts/Interaction/InteractableItem.cs
@@ -1,40 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class InteractableItem : MonoBehaviour {
-
     public Rigidbody rigidbody;
 
     private bool currentlyInteracting;
 
+    private float velocityFactor = 20000f;
+    private Vector3 posDelta;
+
+    private float rotationFactor = 400f;
+    private Quaternion rotationDelta;
+    private float angle;
+    private Vector3 axis;
+
     private HandController attachedHand;
 
-    private Transform interactingPoint;
-	// Use this for initialization
-	void Start () {
+    private Transform interactionPoint;
 
+    // Use this for initialization
+    void Start() {
         rigidbody = GetComponent<Rigidbody>();
-        interactingPoint = new GameObject().transform;
+        interactionPoint = new GameObject().transform;
+        velocityFactor /= rigidbody.mass;
+        rotationFactor /= rigidbody.mass;
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {
+
+        bool isButtonAndLever = this.tag == "button" || this.tag == "lever";
+
+        if (attachedHand && currentlyInteracting && !isButtonAndLever) {                    //pick up object 
+            posDelta = attachedHand.transform.position - interactionPoint.position;
+            this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+            rotationDelta = attachedHand.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+            rotationDelta.ToAngleAxis(out angle, out axis);
+
+            if (angle > 180) {
+                angle -= 360;
+            }
+
+            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+        }
+
+
+    }
 
     public void BeginInteraction(HandController hand) {
         attachedHand = hand;
-        interactingPoint.position = hand.transform.position;
-        interactingPoint.rotation = hand.transform.rotation;
-        interactingPoint.SetParent(transform, true);
+        interactionPoint.position = hand.transform.position;
+        interactionPoint.rotation = hand.transform.rotation;
+        interactionPoint.SetParent(transform, true);
 
         currentlyInteracting = true;
     }
 
     public void EndInteraction(HandController hand) {
-
         if (hand == attachedHand) {
             attachedHand = null;
             currentlyInteracting = false;
