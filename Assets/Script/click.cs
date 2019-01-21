8aab9018689cb1cecda435a379564ba3b9f4ce1e
Bbikrampangeni <33446541+Bbikrampangeni@users.noreply.github.com>
2019-01-21 18:26:04 +0200

    normal lighting trigger
    Normal lighting in game scene

--- Assets/Script/click.cs
+++ Assets/Script/click.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class click : MonoBehaviour {
     public AudioClip help;
-    public GameObject definedButton;
+   // public GameObject definedButton;
     public UnityEvent OnClick = new UnityEvent();
 
     // Use this for initialization
     void Start()
     {
-        definedButton = this.gameObject;
+        //definedButton = this.gameObject;
     }
 
     // Update is called once per frame
@@ -22,21 +23,26 @@ public class click : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0))
         {
+
             if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
             {
+                
                 this.gameObject.AddComponent<AudioSource>();
                 this.GetComponent<AudioSource>().clip = help;
                 this.GetComponent<AudioSource>().Play();
 
                 OnClick.Invoke();
+              //  colorlerp = Color.Lerp(Color.red, Color.yellow, Mathf.PingPong(Time.time, 1));
+
             }
         }
     }
 
+    
     public void stopaudio()
     {
         this.gameObject.AddComponent<AudioSource>();
-        this.GetComponent<AudioSource>().clip = help;
+       this.GetComponent<AudioSource>().clip = help;
         this.GetComponent<AudioSource>().Stop();
 
     }
