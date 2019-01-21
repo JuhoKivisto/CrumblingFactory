69e1362256dae788a0f42ca99f9d78e2216fefe0
chungphamhn <chungphamhn@gmail.com>
2019-01-21 15:23:38 +0200

    test

--- Assets/Scripts/Interaction/HandController.cs
+++ Assets/Scripts/Interaction/HandController.cs
@@ -36,7 +36,8 @@ public class HandController : MonoBehaviour {
 
             //handlingObject.transform.LookAt(new Vector3(handlingObject.transform.localPosition.x, this.transform.localPosition.y, this.transform.localPosition.z));
 
-            //handlingObject.GetComponent<Transform>().localPosition = ;
+            handlingObject.GetComponent<Transform>().localPosition = transform.InverseTransformPoint(this.GetComponent<Transform>().position 
+                    + handlingObject.GetComponent<Transform>().localPosition);
 
             Debug.Log(transform.InverseTransformPoint(this.GetComponent<Transform>().position) + handlingObject.GetComponent<Transform>().localPosition);
 
