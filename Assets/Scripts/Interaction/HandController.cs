97a531f48d333d1ef1e1b84d122e7bd8fc833a81
chungphamhn <chungphamhn@gmail.com>
2019-01-25 11:28:23 +0200

    lever test

--- Assets/Scripts/Interaction/HandController.cs
+++ Assets/Scripts/Interaction/HandController.cs
@@ -50,7 +50,7 @@ public class HandController : MonoBehaviour {
             Vector3 temp = (this.GetComponent<Transform>().position - parentOfLever.GetComponent<Transform>().position);
 
             Vector3 newLocalPosition = new Vector3(lever.transform.localPosition.x, 
-                        temp.y / parentOfLever.localScale.y,lever.transform.localPosition.x);
+                        temp.y / parentOfLever.localScale.y, temp.z / (parentOfLever.localScale.z));
 
             cosin = Mathf.Cos(parentOfLever.localEulerAngles.y * pi / 180f);
             sin = Mathf.Sin(parentOfLever.localEulerAngles.y * pi / 180);
