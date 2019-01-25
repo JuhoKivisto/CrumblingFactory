76cd4a4e7db3790a172d63fa45add6a70f9e9f74
chungphamhn <chungphamhn@gmail.com>
2019-01-25 11:45:10 +0200

    lever test

--- Assets/Scripts/Interaction/InteractableItem.cs
+++ Assets/Scripts/Interaction/InteractableItem.cs
@@ -34,5 +34,18 @@ public class InteractableItem : MonoBehaviour {
 
     }
 
+    private void Update() {
+        if (this.typeOfObject == ObjectType.Lever) {
+
+            GameObject cube = this.transform.parent.GetChild(1).gameObject;
+
+            distance = Vector3.Distance(this.GetComponent<Transform>().localPosition, this.transform.parent.transform.GetChild(1).transform.localPosition);
+
+            Debug.Log(cube.transform.localEulerAngles.x);
+            Debug.Log(cube.transform.rotation.x);
+
+        }
+    }
+
 
 }
