8a6a25378f03b8f51e92f4aa69741d6b924e9cbb
Juho <juho.kivisto@edu.turkuamk.fi>
2019-02-13 11:43:05 +0200

    Initial button logic
    -Basic button logic with joints
    -Button has a down state and up state
    -Button has triggers that prevent lifting button from down to up beneath the button

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour {
+    public GameObject hand;
+    public GameObject button;
+    public GameObject triggerDown;
+    public GameObject triggerUp;
+    public GameObject enableCollider;
+    public GameObject disableCollider;
+    public bool active;
+    public bool isEnabled;
+    public string tag;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckIfInsideOfBoundries();
+	}
+    private void OnTriggerEnter(Collider other) {
+        if (active && other == triggerDown.GetComponent<Collider>() && other.tag == tag ) {
+            active = false;
+            print("Button down");
+        }
+        if (!active && other == triggerUp.GetComponent<Collider>() && other.tag == tag) {
+            active = true;
+            print("Button up");
+        }
+        if (other == enableCollider.GetComponent<Collider>()) {
+            print("enable");
+            //button.GetComponent<Collider>().enabled = true;
+        }
+        if (other == disableCollider.GetComponent<Collider>()) {
+            print("disable");
+            button.GetComponent<Collider>().enabled = false;
+        }
+
+
+    }
+
+    private void CheckIfInsideOfBoundries() {
+        if (hand.GetComponent<SphereCollider>().bounds.Intersects(enableCollider.GetComponent<BoxCollider>().bounds) && !isEnabled) {
+            print("enable");
+            isEnabled = true;
+            button.GetComponent<Collider>().enabled = true;
+        }
+        if (hand.GetComponent<SphereCollider>().bounds.Intersects(disableCollider.GetComponent<BoxCollider>().bounds) && isEnabled) {
+            print("disable");
+            isEnabled = false;
+            button.GetComponent<Collider>().enabled = false;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/RbConstraintsLocal.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RbConstraintsLocal : MonoBehaviour {
+
+    //   public Vector3 localVelocity;
+    //   public Rigidbody rigidbody;
+
+
+    //   // Use this for initialization
+    //   void Start () {
+    //       rigidbody = GetComponent<Rigidbody>();
+
+    //}
+
+
+
+    //   void Update() {
+
+    //       localVelocity = transform.InverseTransformDirection(rigidbody.velocity);
+
+    //       localVelocity.x = 0;
+    //       localVelocity.z = 0;
+
+    //       rigidbody.velocity = transform.TransformDirection(localVelocity);
+
+    //   }
+
+    //[Flags]
+    //public enum LockFlags {
+    //    None,
+    //    Horizontal,
+    //    Vertical,
+    //    Both
+    //}
+
+    //[Range(0, 25)]
+    //public float Force = 1;
+
+    //public LockFlags Lock = LockFlags.Both;
+
+    //public Transform Target;
+
+    //private Rigidbody _rigidbody;
+
+    //private Transform _transform;
+
+    //public void Awake() {
+    //    _rigidbody = GetComponent<Rigidbody>();
+    //    _transform = GetComponent<Transform>();
+    //}
+
+    //public void Update() {
+    //    if (Lock == LockFlags.None || Force <= 0) return;
+
+    //    bool lockX = (Lock & LockFlags.Horizontal) == LockFlags.Horizontal;
+    //    bool lockY = (Lock & LockFlags.Vertical) == LockFlags.Vertical;
+
+    //    Vector3 velocity = _transform.InverseTransformDirection(_rigidbody.velocity);
+    //    if (lockX) velocity.x = 0;
+    //    if (lockY) velocity.y = 0;
+    //    _rigidbody.velocity = _transform.TransformDirection(velocity);
+
+    //    Vector3 distance = Target.position - _transform.position;
+    //    Vector3 idealPosition = Target.position - _transform.forward * distance.magnitude;
+    //    Vector3 correction = idealPosition - _transform.position;
+
+    //    correction = _transform.InverseTransformDirection(correction);
+    //    if (!lockX) correction.x = 0;
+    //    if (!lockY) correction.y = 0;
+    //    correction.z = 0;
+    //    correction = _transform.TransformDirection(correction);
+
+    //    _rigidbody.velocity += correction * Force;
+    //}
+
+    [Header("Freeze Local Position")]
+    [SerializeField]
+    bool x;
+    [SerializeField]
+    bool y;
+    [SerializeField]
+    bool z;
+
+    Vector3 localPosition0;    //original local position
+
+    private void Start() {
+        SetOriginalLocalPosition();
+    }
+
+    private void Update() {
+        float x, y, z;
+
+
+        if (this.x)
+            x = localPosition0.x;
+        else
+            x = transform.localPosition.x;
+
+        if (this.y)
+            y = localPosition0.y;
+        else
+            y = transform.localPosition.y;
+
+        if (this.z)
+            z = localPosition0.z;
+        else
+            z = transform.localPosition.z;
+
+
+        transform.localPosition = new Vector3(x, y, z);
+
+    }
+
+    public void SetOriginalLocalPosition() {
+        localPosition0 = transform.localPosition;
+    }
+
+}
