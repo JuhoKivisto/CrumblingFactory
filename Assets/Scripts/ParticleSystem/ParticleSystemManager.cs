86d0c2d7b3094fc50dad297d41b99a6850f68c7a
chungphamhn <chungphamhn@gmail.com>
2019-02-25 21:09:07 +0200

    Save work on 25-2

---
+++ Assets/Scripts/ParticleSystem/ParticleSystemManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemManager : MonoBehaviour {
+
+    public static ParticleSystemManager instance;
+    public GameObject particleSystemPrefab;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public ParticleSystem test(GameObject particleSys, string stopAction) {
+
+        GameObject clone = Instantiate(particleSys);
+
+        ParticleSystem temp = clone.GetComponent<ParticleSystem>();
+        var main = temp.main;
+
+        main.loop = false;
+
+        temp.Play();
+        return temp;
+    }
+}
---
+++ Assets/Scripts/ParticleSystem/callParticleSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class callParticleSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ParticleSystemManager.instance.test(ParticleSystemManager.instance.particleSystemPrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
