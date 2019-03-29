f26b9c9a123146a167cc315458f80ca5cf41917c
Juho <juho.kivisto@edu.turkuamk.fi>
2019-03-29 14:47:03 +0200

    Added teleport script from Sami

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerMovement : MonoBehaviour {
+
+    private Player player;
+
+	void OnEnable ()
+    {
+        player = GetComponent<Player>();
+	}
+	
+	public void TeleportPlayerToPosition(Vector3 toPosition)
+    {
+        if (toPosition == null || player.PlayerStatus.PlayerLaserActive == false)
+            return;
+
+        TeleportPlayer(toPosition);
+    }
+
+    void TeleportPlayer(Vector3 teleportPoint)
+    {
+        if (teleportPoint != Vector3.zero)
+        {
+            Vector3 playerOffset = CalculatePlayerOffset();
+            Vector3 convertedTeleportPoint = player.PlayAreaCenterObj.InverseTransformPoint(teleportPoint);
+            Vector3 newPoint = convertedTeleportPoint + playerOffset;
+
+            transform.position = transform.TransformPoint(newPoint);
+        }
+    }
+
+    Vector3 CalculatePlayerOffset()
+    {
+        Vector3 offset = Vector3.zero;
+        offset.x = player.PlayAreaCenterObj.localPosition.x - Camera.main.transform.localPosition.x;
+        offset.z = player.PlayAreaCenterObj.localPosition.z - Camera.main.transform.localPosition.z;
+
+        return offset;
+    }
+}
