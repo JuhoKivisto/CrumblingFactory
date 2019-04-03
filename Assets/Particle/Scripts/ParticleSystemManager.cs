5e1ea3cd8aa2f2a620d2d29de015d7371b058e71
chungphamhn <chungphamhn@gmail.com>
2019-04-03 15:32:44 +0300

    merge particle manager to prototypeTest create new scence: particleManager

---
+++ Assets/Particle/Scripts/ParticleSystemManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemManager : MonoBehaviour {
+
+    public static ParticleSystemManager instance;
+
+
+    [Header("Particle Name")]
+
+    public GameObject particleSystemPrefab;
+
+    public GameObject Spark;
+
+    public GameObject Dust;
+    private void Awake() {
+        instance = this;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="particleSys">particle prefab</param>
+    /// <param name="stopAction"></param>
+    /// <param name="position">particle needs position</param>
+    /// <param name="direction">up, down...</param>
+    /// <returns></returns>
+    public ParticleSystem PlayParticle(GameObject particleSys, ParticleSystemStopAction stopAction, Vector3 position, Vector3 direction) {
+
+        Quaternion changeDirection = new Quaternion();
+
+
+        if (direction == Vector3.zero) {                                //check that the direction is not zero
+            direction = Vector3.up;                                     //default direction is up
+        }
+        changeDirection = Quaternion.LookRotation(direction);
+        
+
+        GameObject clone = Instantiate(particleSys, position, changeDirection);
+
+        ParticleSystem temp = clone.GetComponent<ParticleSystem>();
+
+        var main = temp.main;
+
+        main.stopAction = stopAction;
+
+        temp.Play();
+
+        if(particleSys.transform.childCount > 0) {
+            foreach(Transform child in particleSys.transform) {
+
+                GameObject childGameObject = child.gameObject;
+
+                ParticleSystem tempChild = childGameObject.GetComponent<ParticleSystem>();
+
+                var mainChild = tempChild.main;
+
+                mainChild.stopAction = stopAction;
+
+                tempChild.Play();
+            }
+        }
+
+        return temp;
+    }
+
+    public void stopParticleSystem (ParticleSystem particleSystem) {
+
+        if(particleSystem.gameObject.transform.childCount > 0) {
+            foreach (Transform child in particleSystem.gameObject.transform) {
+                ParticleSystem childTemp = child.gameObject.GetComponent<ParticleSystem>();
+
+                childTemp.Stop();
+            }
+        }
+        particleSystem.Stop();
+    }
+}
