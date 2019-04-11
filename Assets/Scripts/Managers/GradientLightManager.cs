484b5bf51a4f18b8d4dcbc49f535a43ac7150924
chungphamhn <chungphamhn@gmail.com>
2019-04-11 13:37:38 +0300

    Add GradientManager, make small spark

---
+++ Assets/Scripts/Managers/GradientLightManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientLightManager : MonoBehaviour
+{
+    /// <summary>
+    /// This scipt changes light color according to heat so that it is more red when heat increases 
+    /// </summary>
+    /// 
+
+
+    [SerializeField]
+    private Gradient grad;
+
+    [SerializeField]
+    AnimationCurve intensity;
+
+    [SerializeField]
+    List<Light> lights;
+
+
+    private float heatNormalizeNumber;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        heatNormalizeNumber = (HeatManager.instance.Heat - Stats.instance.minHeat) / (Stats.instance.maxHeat - Stats.instance.minHeat);
+
+        foreach (Light light in lights) {
+
+            if(light != null) {
+
+                light.color = grad.Evaluate(heatNormalizeNumber);
+                light.intensity = intensity.Evaluate(heatNormalizeNumber * intensity.keys[intensity.length - 1].value);
+            }
+            
+        }       
+
+    }
+}
