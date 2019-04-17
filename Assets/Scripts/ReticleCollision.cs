1bd64008c91d26afdfe490e943ac4c8bb50db20d
Juho <juho.kivisto@edu.turkuamk.fi>
2019-04-17 16:52:44 +0300

    Rearranged assets and scene additions
    	-Added more pipes
    	-Reordered folders and assets

---
+++ Assets/Scripts/ReticleCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        LaserPointer.reticleCollides = true;
+        Debug.Log("reticleCollides is now true");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        LaserPointer.reticleCollides = false;
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+            ParticleManager.instance.PlayParticle(ParticleManager.instance.Spark, ParticleSystemStopAction.Destroy, ParticleManager.instance.SparkPosition[0], Vector3.up);
+    }
+}
