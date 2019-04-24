905d1687866de6c0ae4d2069a74e01901abc065f
chungphamhn <chungphamhn@gmail.com>
2019-04-24 14:25:36 +0300

    GradientMangaer update, control the heat meter neele

---
+++ Assets/Scripts/GradientLightManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientLightManager : MonoBehaviour {
+    /// <summary>
+    /// This scipt changes light color according to heat so that it is more red when heat increases 
+    /// </summary>
+    /// 
+    public static GradientLightManager instance;
+
+    [SerializeField]
+    private Gradient grad;
+
+    [SerializeField]
+    AnimationCurve intensity;
+
+    [SerializeField]
+    List<Light> lights;
+
+    public float Heat;
+    public float minHeat;
+    public float maxHeat;
+    public float heatNormalizeNumber;
+
+
+    private void Start() {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+
+            GradientLightManager.instance.callCoroutine();
+            Debug.Log(heatNormalizeNumber);
+        }
+        heatNormalizeNumber = (Heat - minHeat) / (maxHeat - minHeat);
+    }
+
+    public void callCoroutine() {
+        StartCoroutine(gradientLight());
+    }
+
+    IEnumerator gradientLight() {
+        while (true) {
+
+            foreach (Light light in lights) {
+
+                if (light != null) {
+
+                    light.color = grad.Evaluate(heatNormalizeNumber);
+                    light.intensity = intensity.Evaluate(heatNormalizeNumber * intensity.keys[intensity.length - 1].value);//intensity.keys[intensity.length - 1].value - to get the biggest value of intensity
+                }
+            }
+            yield return null;
+        }
+        
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject cube;
+
+    public float Heat;
+    public float minHeat;
+    public float maxHeat;
+    public float heatNormalizeNumber;
+
+    // Start is called before the first frame update
+    private void Start() {
+        StartCoroutine(heatMeterNeedle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            heatNormalizeNumber = (Heat - minHeat) / (maxHeat - minHeat);
+
+        //heatNormalizeNumber = (Heat - minHeat) / (maxHeat - minHeat);
+        ////cube.transform.rotation.z = 1;
+
+        //cube.transform.localEulerAngles = new Vector3(cube.transform.localEulerAngles.x, cube.transform.localEulerAngles.y, - heatNormalizeNumber * 180);
+        //Debug.Log(heatNormalizeNumber);
+        //Debug.Log(cube.transform.rotation);
+    }
+
+    IEnumerator heatMeterNeedle() {
+
+        while (true) {
+
+            //cube.transform.rotation.z = 1;
+
+            cube.transform.localEulerAngles = new Vector3(cube.transform.localEulerAngles.x, cube.transform.localEulerAngles.y, -heatNormalizeNumber * 180);
+            Debug.Log(heatNormalizeNumber);
+
+            yield return null;
+        }
+    }
+}
